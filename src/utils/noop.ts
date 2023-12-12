dcad0553a031c2143f450d1662967d7a6fac5cb8
ylandau <ylandau@paloaltonetworks.com>
2023-12-12 11:52:26 +0200

    Add test packages and upgrade to vite 5

---
+++ src/utils/noop.ts
@@ -0,0 +1,4 @@
+/* c8 ignore start */
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export function noop(): void {}
+/* c8 ignore stop */
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  test: {
+    global: true,
+    environment: "jsdom",
+    setupFile: "./setupTests.ts",
+  },
 });
